fd70ca5647dfaececf534f2ca70be9ed31da52fc
avdkishore <avdkishore@gmail.com>
2019-08-30 13:07:04 +0530

    Support keys for sound

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
   const sounds = document.querySelectorAll('.sound');
-  const pads = document.querySelectorAll('.pads div');
+  const pads = document.querySelectorAll('.pads > div');
   const visual = document.querySelector('.visual');
   const colors = [
     '#60d394',
@@ -11,7 +11,9 @@ window.addEventListener('load', () => {
     '#0badca',
   ];
 
-  function  createBubbles(index) {
+  const keys = ['S','D', 'F', 'J', 'K', 'L'];
+
+  function createBubbles(index) {
     const bubble = document.createElement('div');
     visual.appendChild(bubble);
     bubble.style.backgroundColor = colors[index];
@@ -21,14 +23,28 @@ window.addEventListener('load', () => {
       visual.removeChild(this);
     });
   }
+
+  function play(index) {
+    sounds[index].currentTime = 0;
+    sounds[index].play();
+
+    createBubbles(index);
+  }
   
   // sound
   pads.forEach((pad, i) => {
     pad.addEventListener('click', function() {
-      sounds[i].currentTime = 0;
-      sounds[i].play();
-      
-      createBubbles(i);
+      play(i);
     });
   });
+
+  addEventListener('keyup', (event) => {
+    const key = event.key.toUpperCase();
+
+    const keyIndex = keys.indexOf(key);
+
+    if (keyIndex !== -1) {
+      play(keyIndex);
+    }
+  })
 });
